47d0b5808fdd75dae70975bf15c0988bf946216d
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-11-09 17:50:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    string minSum(vector<int> &arr) {
+        // code here
+        string n1, n2;
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(i % 2 == 0) n1 += to_string(arr[i]);
+            else n2 += to_string(arr[i]);
+        }
+        reverse(n1.begin(), n1.end());
+        reverse(n2.begin(), n2.end());
+        int i = 0, j = 0, carry = 0, n = n1.size(), m = n2.size();
+        string ans;
+        while(i < n || j < m)
+        {
+            int sum = 0;
+            if(i < n) sum += n1[i++] - '0';
+            if(j < m) sum += n2[j++] - '0';
+            sum += carry;
+            carry = sum / 10;
+            sum %= 10;
+            ans += to_string(sum);
+        }
+        ans += to_string(carry);
+        while(ans.back() == '0') ans.pop_back();
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        string ans = ob.minSum(a);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
