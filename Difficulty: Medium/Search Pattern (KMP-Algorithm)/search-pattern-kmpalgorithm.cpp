f963c5d5ec2b3a4da7daec2828319859b92fc8cb
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-12-02 15:50:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
+++ Difficulty: Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -1,84 +1,78 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-        void lpsfind(vector<int> &lps, string s)
+class Solution {
+  public:
+    void lpsfind(vector<int> &lps, string s)
+    {
+        int pre = 0, suf = 1;
+        while(suf < s.size())
         {
-            int pre = 0, suf = 1;
-            while(suf < s.size())
+            if(s[pre] == s[suf])
             {
-                if(s[pre] == s[suf])
+                lps[suf] = pre + 1;
+                suf++, pre++;
+            }
+            else
+            {
+                if(pre == 0)
                 {
-                    lps[suf] = pre + 1;
-                    suf++, pre++;
+                    lps[suf] = 0;
+                    suf++;
                 }
-                else
-                {
-                    if(pre == 0)
-                    {
-                        lps[suf] = 0;
-                        suf++;
-                    }
-                    else pre = lps[pre - 1];
-                }
+                else pre = lps[pre - 1];
             }
         }
-        vector <int> search(string pat, string txt)
+    }
+    
+    vector<int> search(string& pat, string& txt) {
+        // code here
+        vector<int> lps(pat.size(), 0);
+        lpsfind(lps, pat);
+        int first = 0, second = 0;
+        vector<int> ans;
+        while(first < txt.size())
         {
-            //code here
-            vector<int> lps(pat.size(), 0);
-            lpsfind(lps, pat);
-            int first = 0, second = 0;
-            vector<int> ans;
-            while(first < txt.size() && second < pat.size())
+            if(txt[first] == pat[second])
             {
-                if(txt[first] == pat[second])
+                first++, second++;
+                if(second == pat.size())
                 {
-                    first++, second++;
-                    if(second == pat.size())
-                    {
-                        ans.push_back(first - second + 1);
-                        second = lps[second - 1];
-                    }
-                }
-                else
-                {
-                    if(second == 0) first++;
-                    else second = lps[second - 1];
+                    ans.push_back(first - second);
+                    second = lps[second - 1];
                 }
             }
-            if(ans.size() > 0) return ans;
-            else return {-1};
+            else
+            {
+                if(second == 0) first++;
+                else second = lps[second - 1];
+            }
         }
-     
+        return ans;
+    }
 };
 
 //{ Driver Code Starts.
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         string S, pat;
         cin >> S >> pat;
         Solution ob;
-        vector <int> res = ob.search(pat, S);
-        if (res.size()==0)
-            cout<<-1<<endl;
+        vector<int> res = ob.search(pat, S);
+        if (res.size() == 0)
+            cout << "[]" << endl;
         else {
-            for (int i : res) cout << i << " ";
+            for (int i : res)
+                cout << i << " ";
             cout << endl;
         }
     }
     return 0;
 }
-
-// Contributed By: Pranay Bansal
 
 // } Driver Code Ends
