f607043c41c5a6631584370be9a71fd3278d8ac7
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2024-12-31 09:40:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -8,30 +8,23 @@ using namespace std;
 // } Driver Code Ends
 // User function template for C++
 
-// User function template for C++
-
 class Solution {
   public:
     // Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M) {
+    int celebrity(vector<vector<int> >& mat) {
         // code here
-        int n = M.size();
+        int n = mat.size();
         stack<int> st;
-        for(int i = n-1; i >= 0 ;i--)
-        st.push(i);
-        
+        for(int i = n - 1; i >= 0; i--) st.push(i);
         int first, second;
-        
         while(st.size() > 1)
         {
             first = st.top();
             st.pop();
             second = st.top();
             st.pop();
-            if(M[first][second] && !M[second][first])
-            st.push(second);
-            else if(!M[first][second] && M[second][first])
-            st.push(first);
+            if(mat[first][second] && !mat[second][first]) st.push(second);
+            else if(!mat[first][second] && mat[second][first]) st.push(first);
         }
         
         if(st.empty()) return -1;
@@ -40,12 +33,13 @@ class Solution {
         st.pop();
         
         int row = 0, col = 0;
-        for(int i=0;i<n;i++)
+        for(int i = 0; i < n; i++)
         {
-            row += M[num][i];
-            col += M[i][num];
+            row += mat[num][i];
+            col += mat[i][num];
         }
-        return row == 0 && col == n-1 ? num : -1;
+        
+        return row == 0 && col == n - 1 ? num : -1;
     }
 };
 
@@ -65,6 +59,7 @@ int main() {
         }
         Solution ob;
         cout << ob.celebrity(M) << endl;
+        cout << "~" << endl;
     }
 }
 
