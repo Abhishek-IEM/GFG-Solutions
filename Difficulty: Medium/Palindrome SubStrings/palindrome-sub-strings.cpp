42508540a6355169c9fed77b5b61697eb86f39cb
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-03-09 09:53:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int length(int left, int right, string &s)
+    {
+        int count = 0;
+        while(left >= 0 && right < s.size())
+        {
+            if(s[left] == s[right])
+            {
+                count++;
+                left--;
+                right++;
+            }
+            else break;
+        }
+        return count;
+    }
+    
+    int countPS(string &s) {
+        // code here
+        int n = s.size(), count = 0;
+        //odd 
+        for(int i = 0; i < n; i++)
+        {
+            count += length(i - 1, i + 1, s);
+        }
+        //even 
+        for(int i = 1; i < n; i++)
+        {
+            count += length(i - 1, i, s);
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.countPS(s) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
