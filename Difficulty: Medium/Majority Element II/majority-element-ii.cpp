b1c4f93f040d9c8a3975a91d52853654ade756e0
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-03-25 21:06:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.cpp
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to find the majority elements in the array
@@ -13,16 +14,19 @@ class Solution {
         vector<int> ans;
         unordered_map<int, int> mp;
         for(int i = 0; i < n; i++) mp[arr[i]]++;
-        for(auto x : mp)
+        for(auto &x : mp)
         {
-            int a = x.first;
-            int b = x.second;
-            if(b > n/3) ans.push_back(a);
+            int a = x.first, b = x.second;
+            if(b > n / 3)
+            {
+                ans.push_back(a);
+            }
         }
         sort(ans.begin(), ans.end());
         return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
