ad133c532adabd14977a685356dded8ec1831eaf
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-03-28 10:53:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -5,27 +5,29 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // arr: input array
     // Function to find maximum circular subarray sum.
     int circularSubarraySum(vector<int> &arr) {
-        // your code here
-        int maxSum = INT_MIN, minSum = INT_MAX, n = arr.size();
-        int s1 = 0, s2 = 0, totalSum = 0;
-        for(int i = 0; i < n; i++)
-        {
-            totalSum += arr[i];
+        int minSum = INT_MAX;
+        int maxSum = INT_MIN;
+        int s1 = 0, s2 = 0, TotalSum = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            TotalSum += arr[i];
             s1 += arr[i];
             s2 += arr[i];
-            if(s1 < 0) s1 = 0; //maximum Sum
-            if(s2 > 0) s2 = 0; //minimum Sum
+            if(s1 < 0) s1 = 0;
+            if(s2 > 0) s2 = 0;
             maxSum = max(maxSum, s1);
             minSum = min(minSum, s2);
         }
-        return max(maxSum, totalSum - minSum);
+        return max(maxSum, TotalSum - minSum);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
@@ -48,6 +50,7 @@ int main() {
         int res = ob.circularSubarraySum(arr);
 
         cout << res << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
