42d66752029cfe0d23e672c640eadee8b1eaa8ee
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-04-02 17:58:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.cpp
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -4,28 +4,29 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     int myAtoi(char *s) {
         // Your code here
-        int i = 0, sign = 1;
-        long ans = 0;
-        while(s[i] == ' ') i++;
-        while(s[i] == '+' || s[i] == '-')
-        {
-            sign = (s[i] == '-') ? -1 : 1;
+        int i=0, sign=1;
+        long ans=0;
+        
+        while(s[i]==' ') i++;
+        if(s[i]=='+'|| s[i]=='-'){
+            sign=(s[i]=='-')?-1:1;
             i++;
         }
-        while(s[i] >= '0' && s[i] <= '9')
-        {
-            ans = ans * 10 + (s[i] - '0');
-            if(sign * ans >= INT_MAX) return INT_MAX;
-            if(sign * ans <= INT_MIN) return INT_MIN;
+        while(s[i]>='0' && s[i]<='9'){
+            ans=ans*10+(s[i]-'0');
+            if(sign*ans>=INT_MAX) return INT_MAX;
+            if(sign*ans<=INT_MIN) return INT_MIN;
             i++;
         }
-        return sign * ans;
+        return (int)sign*ans; 
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
