1ffb123f5f5a79b43892fa5982ea35bd0b85a52d
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-04-08 19:24:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -5,17 +5,51 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
     // Function to check if two strings are rotations of each other or not.
     bool areRotations(string &s1, string &s2) {
         // Your code here
-        string ans = s1;
-        ans += s1;
-        if(ans.find(s2) != -1) return true;
+        if (s1.length() != s2.length()) return false;
+
+        string combined = s1 + s1;
+    
+        vector<int> lps(s2.size(), 0);
+        int len = 0, i = 1;
+        while (i < s2.size()) {
+            if (s2[i] == s2[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    
+        i = 0;
+        int j = 0;
+        while (i < combined.size()) {
+            if (combined[i] == s2[j]) {
+                i++;
+                j++;
+            }
+            if (j == s2.size()) return true;
+            else if (i < combined.size() && combined[i] != s2[j]) {
+                if (j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+    
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 
