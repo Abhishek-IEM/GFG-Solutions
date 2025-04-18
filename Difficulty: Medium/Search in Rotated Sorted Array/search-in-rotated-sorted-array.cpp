b37e33067efbb082b7b3b7647291d93da5185672
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-04-18 18:55:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -4,31 +4,38 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     int search(vector<int>& arr, int key) {
-        // complete the function here
-        int start = 0, end = arr.size() - 1;
-        while(start <= end)
-        {
-            int mid = start + (end - start)/2;
-            if(arr[mid] == key) return mid;
-            if(arr[start] <= arr[mid]) //left side sorted
-            {
-                if(key >= arr[start] && key < arr[mid])
-                end = mid - 1;
-                else start = mid + 1;
+        // Code Here
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+    
+            if (arr[mid] == key) return mid;
+            
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= key && key < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
-            else
-            {
-                if(key > arr[mid] && key <= arr[end])
-                start = mid + 1;
-                else end = mid - 1;
+            else {
+                if (arr[mid] < key && key <= arr[high]) {
+                    low = mid + 1; 
+                } else {
+                    high = mid - 1; 
+                }
             }
         }
+    
         return -1;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
