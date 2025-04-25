13bb84fef5f3b114e1710021d11eaaaa42090009
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-04-25 18:57:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -5,38 +5,31 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
     // Function to rotate matrix anticlockwise by 90 degrees.
-     void rotate(vector<vector<int> >& matrix) {
-        int n = matrix.size();
-        //transpose
-        for(int i = 0; i < n - 1; i++)
-        {
-            for(int j = i + 1; j < n; j++)
-            {
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        //rowwise reverse
-        for(int i = 0; i < n; i++)
-        {
-            int start = 0, end = n - 1;
-            while(start < end)
-            {
-                swap(matrix[i][start], matrix[i][end]);
-                start++;
-                end--;
-            }
-        }
-    }
-    void rotateby90(vector<vector<int>>& matrix) {
+    void rotateby90(vector<vector<int>>& mat) {
         // code here
-        rotate(matrix);
-        rotate(matrix);
-        rotate(matrix);
+        int n = mat.size();
+    
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+    
+        for (int j = 0; j < n; ++j) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                swap(mat[top][j], mat[bottom][j]);
+                ++top;
+                --bottom;
+            }
+        }
     }
 };
+
 
 
 //{ Driver Code Starts.
