213cdeccf6bd84811e3a59cd90aefe3a9a29f944
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-04-27 17:13:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.cpp
@@ -6,19 +6,20 @@ using namespace std;
 // } Driver Code Ends
 
 
+
 class Solution {
   public:
     // Function to search a given number in row-column sorted matrix.
     bool searchRowMatrix(vector<vector<int>> &mat, int x) {
         // code here
-        int n = mat.size(), m = mat[0].size();
-        for(int i = 0; i < n; i++)
-        {
-            if(binary_search(mat[i].begin(), mat[i].end(), x)) return true;
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            if(binary_search(mat[i].begin(),mat[i].end(),x)) return true;
         }
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
