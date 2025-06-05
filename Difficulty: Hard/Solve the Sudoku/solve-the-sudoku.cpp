c225d1a27563084e652982281e7596dc4a245cde
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-06-05 12:06:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to find a solved Sudoku.
@@ -55,45 +49,3 @@ class Solution {
         solve(mat);
     }
 };
-
-//{ Driver Code Starts.
-
-vector<int> inputLine() {
-    string str;
-    getline(cin, str);
-    stringstream ss(str);
-    int num;
-    vector<int> res;
-    while (ss >> num) {
-        res.push_back(num);
-    }
-    return res;
-}
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<vector<int>> grid;
-        for (int i = 0; i < 9; i++) {
-            vector<int> v = inputLine();
-            grid.push_back(v);
-        }
-
-        Solution ob;
-
-        ob.solveSudoku(grid);
-
-        for (auto v : grid) {
-            for (auto elem : v) {
-                cout << elem << " ";
-            }
-            cout << endl;
-        }
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
