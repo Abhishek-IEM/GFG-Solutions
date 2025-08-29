dc8dbda72730833563b2516a483e2c324eec25e1
Abhishek Sharma <119843012+Abhishek-IEM@users.noreply.github.com>
2025-08-29 08:54:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -0,0 +1,53 @@
+
+class Solution {
+  public:
+    // Function to find the smallest window in the string s1 consisting
+    // of all the characters of string s2.
+    string smallestWindow(string &s, string &t) {
+        // Your code here
+        int total = t.size();
+        int n = s.size();
+        unordered_map<char, int> m;
+        
+        for(int i = 0; i < t.size(); i++)
+        {
+            m[t[i]]++;
+        }
+        
+        int start = 0, end = 0, ans = INT_MAX, index = -1;
+        
+        while(end < s.size())
+        {
+            m[s[end]]--;
+            
+            if(m[s[end]] >= 0) total--;
+            while(!total && start <= end)
+            {
+                if(ans > end - start + 1)
+                {
+                    ans = end - start + 1;
+                    index = start;
+                }
+                
+                m[s[start]]++;
+                
+                if(m[s[start]] > 0) total++;
+                
+                start++;
+            }
+            end++;
+        }
+        
+        if(index == -1) return "";
+        
+        string str = "";
+        
+        for(int i = index; i < index + ans; i++)
+        {
+            str += s[i];
+        }
+        
+        return str;
+    }
+};
+
